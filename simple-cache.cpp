9a8a4cf38621d51b0798d8dabb6e64974f442a56
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-03-11 13:39:28 +0700

    refac (cache): constant mounting point

--- simple-cache.cpp
+++ simple-cache.cpp
@@ -8,28 +8,23 @@
 #include <stdexcept>
 
 namespace cache {
-  struct Key {
-    std::string mountingPoint;
-    off_t offset;
-
-    Key(std::string mountingPoint, off_t offset) : mountingPoint(std::move(mountingPoint)), offset(offset) {}
-  };
+  const char *const mountingPoint = "/dev/sda"; // change this to the path of the file you want to cache
 
   class Cache {
   private:
     int capacity = 0;
-    std::list<std::pair<Key, char>> cacheList;
-    std::unordered_map<Key, typename std::list<std::pair<Key, char>>::iterator> cacheMap;
+    std::list<std::pair<off64_t, char>> cacheList;
+    std::unordered_map<off64_t, typename std::list<std::pair<off64_t, char>>::iterator> cacheMap;
 
-    static char readFromDisk(const Key& key) {
-      int fd = open(key.mountingPoint.c_str(), O_RDONLY);
+    static char readFromDisk(const off64_t offset) {
+      int fd = open(mountingPoint, O_RDONLY);
       if (fd == -1) {
         perror("Error opening file");
         throw std::runtime_error("Error opening file");
       }
 
       char temp[1];
-      ssize_t bytesRead = pread(fd, temp, 1, key.offset);
+      ssize_t bytesRead = pread(fd, temp, 1, offset);
       if (bytesRead == -1) {
         perror("Error reading file");
         throw std::runtime_error("Error reading file");
@@ -40,22 +35,22 @@ namespace cache {
     }
 
   public:
-    char get(Key key) {
-      if (cacheMap.find(key) == cacheMap.end()) {
+    Cache() : capacity(0) {}
+    Cache(int capacity) : capacity(capacity) {}
+
+    char get(const off64_t offset) {
+      if (cacheMap.find(offset) == cacheMap.end()) {
         if (cacheList.size() == capacity) {
           auto last = cacheList.back();
           cacheList.pop_back();
           cacheMap.erase(last.first);
         }
-        char value = readFromDisk(key);
-        cacheList.emplace_front(key, value);
-        cacheMap[key] = cacheList.begin();
-        return value;
+        cacheList.push_front(std::make_pair(offset, readFromDisk(offset)));
+        cacheMap[offset] = cacheList.begin();
       } else {
-        auto it = cacheMap[key];
-        cacheList.splice(cacheList.begin(), cacheList, it);
-        return it->second;
+        cacheList.splice(cacheList.begin(), cacheList, cacheMap[offset]);
       }
+      return cacheMap[offset]->second;
     }
   };
 }
