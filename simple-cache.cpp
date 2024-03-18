48932101af3565d57a494bbfca5a7fea2e83db70
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-03-18 09:01:18 +0700

    refactor: refactor the cache to be inside main directly

--- simple-cache.cpp
+++ simple-cache.cpp
@@ -5,51 +5,45 @@
 #include <fcntl.h>
 #include <cstdio>
 #include <stdexcept>
+#include <vector>
 
-namespace cache {
-  class Cache {
-  private:
-    const char *const mountingPoint;
-    const int capacity = 0;
-    std::list<std::pair<off64_t, char>> cacheList;
-    std::unordered_map<off64_t, typename std::list<std::pair<off64_t, char>>::iterator> cacheMap;
+int main() {
+  const char *const mountingPoint = "/dev/sda";
+  const int capacity = 50;
+  std::list<std::pair<off64_t, char>> cacheList;
+  std::unordered_map<off64_t, typename std::list<std::pair<off64_t, char>>::iterator> cacheMap;
 
-    char readFromDisk(const off64_t offset) {
+  int hit = 0;
+  std::vector<off64_t> offset;
+
+  for (long x : offset) {
+    if (cacheMap.find(x) == cacheMap.end()) {
+      if (cacheMap.size() == capacity) {
+        auto last = cacheList.back();
+        cacheList.pop_back();
+        cacheMap.erase(last.first);
+      }
+      char temp[1];
       int fd = open(mountingPoint, O_RDONLY);
       if (fd == -1) {
         perror("Error opening file");
         throw std::runtime_error("Error opening file");
       }
-
-      char temp[1];
-      ssize_t bytesRead = pread(fd, temp, 1, offset);
+      ssize_t bytesRead = pread(fd, temp, 1, x);
       if (bytesRead == -1) {
         perror("Error reading file");
         throw std::runtime_error("Error reading file");
       }
       close(fd);
 
-      return temp[0];
+      cacheList.emplace_front(x, temp[0]);
+      cacheMap[x] = cacheList.begin();
+    } else {
+      cacheList.splice(cacheList.begin(), cacheList, cacheMap[x]);
+      cacheMap[x] = cacheList.begin();
+      hit++;
     }
+  }
 
-  public:
-    Cache() : capacity(0), mountingPoint("/dev/sda") {}
-    Cache(int capacity, const char* mountingPoint) : capacity(capacity), mountingPoint(mountingPoint) {}
-
-    char get(const off64_t offset) {
-      if (cacheMap.find(offset) == cacheMap.end()) {
-        if (cacheList.size() == capacity) {
-          auto last = cacheList.back();
-          cacheList.pop_back();
-          cacheMap.erase(last.first);
-        }
-        cacheList.emplace_front(offset, readFromDisk(offset));
-        cacheMap[offset] = cacheList.begin();
-      } else {
-        cacheList.splice(cacheList.begin(), cacheList, cacheMap[offset]);
-        cacheMap[offset] = cacheList.begin();
-      }
-      return cacheMap[offset]->second;
-    }
-  };
+  return 0;
 }
