247f38cb2ff6f57b07619f36bb6f0a50e2b79c80
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-03-21 17:12:04 +0700

    feat (LFU): change from LRU to LFU, the hit ratio is still the same (I guess, it's because of the trace)

--- simple-cache.cpp
+++ simple-cache.cpp
@@ -7,13 +7,19 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <algorithm>
+
+struct {
+  int frequency;
+  char data;
+} typedef Cache;
 
 int main() {
   const char *const mountingPoint = "/dev/sda";
   const long capacity = 100; // capacity of the cache in bytes (currently)
   char temp[1]; // temporary buffer for reading bytes from the disk
-  std::list<char> cacheList; // the list of cached bytes
-  std::unordered_map<off64_t, typename std::list<char>::iterator> cacheMap; // point an offset to the corresponding byte in the cache
+  std::list<Cache> cacheList; // the list of cached bytes
+  std::unordered_map<off64_t, typename std::list<Cache>::iterator> cacheMap; // point an offset to the corresponding byte in the cache
   // NOTE: the cacheMap is a map of iterators, not values to allow flexibility in the cacheList later on
 
   // hit count and total I/O operations
@@ -54,11 +60,13 @@ int main() {
     for(int i = 0; i < size; i++){
       // if the byte is not in the cache, read it from the disk and add it to the cache
       if (cacheMap.find(offset+i) == cacheMap.end()) {
-        // if the cache is full, remove the last element
+        // if the cache is full, remove the least frequently used elemen
         if (cacheMap.size() == capacity) {
-          auto last = cacheList.back();
-          cacheList.pop_back();
-          cacheMap.erase(last);
+          auto last = std::min_element(cacheList.begin(), cacheList.end(), [](const Cache& a, const Cache& b) {
+            return a.frequency < b.frequency;
+          });
+          cacheMap.erase(last->frequency);
+          cacheList.erase(last);
         }
 
         // read the byte from the disk
@@ -68,13 +76,12 @@ int main() {
           throw std::runtime_error("Error reading file");
         }
 
-        // add the byte to the cache (at the front of the list for the LRU policy)
-        cacheList.emplace_front(temp[0]);
+        // add the cached data to the cacheList
+        cacheList.emplace_front(Cache{1, temp[0]});
         cacheMap[offset+i] = cacheList.begin();
       } else {
-        // if the byte is in the cache, move it to the front of the list
-        cacheList.splice(cacheList.begin(), cacheList, cacheMap[offset+i]);
-        cacheMap[offset+i] = cacheList.begin();
+        // if the byte is in the cache, add its frequency
+        cacheMap[offset+i]->frequency++;
         hit++;
       }
       total++;
@@ -86,7 +93,7 @@ int main() {
 
   // calculating the hit ratio
   double hitRatio = double(hit) / double(total);
-  std::cout << "LRU Hit Ratio: " << hitRatio << std::endl;
+  std::cout << "LFU Hit Ratio: " << hitRatio << std::endl;
 
   return 0;
 }
