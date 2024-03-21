c08d22daf145cfb9b82a5ac310fc2b49da7818a9
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-03-21 17:04:37 +0700

    refactor (LRU): simplify and add comment to the LRU cache

--- simple-cache.cpp
+++ simple-cache.cpp
@@ -1,6 +1,5 @@
 #include <unordered_map>
 #include <list>
-#include <utility>
 #include <unistd.h>
 #include <fcntl.h>
 #include <cstdio>
@@ -11,18 +10,30 @@
 
 int main() {
   const char *const mountingPoint = "/dev/sda";
-  const int capacity = 1000000;
-  std::list<std::pair<off64_t, char>> cacheList;
-  std::unordered_map<off64_t, typename std::list<std::pair<off64_t, char>>::iterator> cacheMap;
+  const long capacity = 100; // capacity of the cache in bytes (currently)
+  char temp[1]; // temporary buffer for reading bytes from the disk
+  std::list<char> cacheList; // the list of cached bytes
+  std::unordered_map<off64_t, typename std::list<char>::iterator> cacheMap; // point an offset to the corresponding byte in the cache
+  // NOTE: the cacheMap is a map of iterators, not values to allow flexibility in the cacheList later on
 
+  // hit count and total I/O operations
   int hit = 0;
   int total = 0;
 
+  // try to open the trace file
   std::ifstream traceFile("msr.cut.per_10k.rw_40_60.1370.trace");
   if (!traceFile.is_open()) {
     throw std::runtime_error("Error opening trace file");
   }
 
+  // access the mounting point
+  int fd = open(mountingPoint, O_RDONLY);
+  if (fd == -1) {
+    perror("Error opening file");
+    throw std::runtime_error("Error opening file");
+  }
+
+  // read the trace file line by line
   std::string line;
   while (std::getline(traceFile, line)) {
     std::istringstream iss(line);
@@ -30,37 +41,38 @@ int main() {
     int deviceId, size, rw;
     off64_t offset;
 
+    // dissect the line into the variables
     if (!(iss >> timestamp >> deviceId >> offset >> size >> rw)) {
       throw std::runtime_error("Error parsing trace file");
     }
 
+    // only consider read operations (rw == 1)
     if (rw != 1) {
       continue;
     }
 
     for(int i = 0; i < size; i++){
+      // if the byte is not in the cache, read it from the disk and add it to the cache
       if (cacheMap.find(offset+i) == cacheMap.end()) {
+        // if the cache is full, remove the last element
         if (cacheMap.size() == capacity) {
           auto last = cacheList.back();
           cacheList.pop_back();
-          cacheMap.erase(last.first);
+          cacheMap.erase(last);
         }
-        char temp[1];
-        int fd = open(mountingPoint, O_RDONLY);
-        if (fd == -1) {
-          perror("Error opening file");
-          throw std::runtime_error("Error opening file");
-        }
+
+        // read the byte from the disk
         ssize_t bytesRead = pread(fd, temp, 1, offset+i);
         if (bytesRead == -1) {
           perror("Error reading file");
           throw std::runtime_error("Error reading file");
         }
-        close(fd);
 
-        cacheList.emplace_front(offset+i, temp[0]);
+        // add the byte to the cache (at the front of the list for the LRU policy)
+        cacheList.emplace_front(temp[0]);
         cacheMap[offset+i] = cacheList.begin();
       } else {
+        // if the byte is in the cache, move it to the front of the list
         cacheList.splice(cacheList.begin(), cacheList, cacheMap[offset+i]);
         cacheMap[offset+i] = cacheList.begin();
         hit++;
@@ -69,8 +81,12 @@ int main() {
     }
   }
 
+  // close the access to the mounting point
+  close(fd);
+
+  // calculating the hit ratio
   double hitRatio = double(hit) / double(total);
-  std::cout << "Hit Ratio: " << hitRatio << std::endl;
+  std::cout << "LRU Hit Ratio: " << hitRatio << std::endl;
 
   return 0;
 }
